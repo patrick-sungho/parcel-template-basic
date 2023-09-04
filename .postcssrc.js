92ce490bad775946f5e260e38151c66c851f7bd2
patrick-sungho <leesungho415@naver.com>
2023-09-04 12:11:28 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
+
+// ESM
+// import, export
+
+// CommonJs
+// import autoprefixer from 'autopprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export
+/*
+  export {
+    plugins: [
+      autoprefixer
+    ]
+  }
+*/
